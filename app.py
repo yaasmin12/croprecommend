8d36c80a199dd651bdea97e043b8188ccf7326e5
farmanhaider123 <97297440+farmanhaider123@users.noreply.github.com>
2022-03-08 21:15:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ disease_classes = ['Apple___Apple_scab',
                    'Tomato___Tomato_mosaic_virus',
                    'Tomato___healthy']
 
-disease_model_path = 'models/plant_disease_model.pth'
+disease_model_path = '/farmanhaider123/Harvestify/blob/deploy/models/plant_disease_model.pth'
 disease_model = ResNet9(3, len(disease_classes))
 disease_model.load_state_dict(torch.load(
     disease_model_path, map_location=torch.device('cpu')))
